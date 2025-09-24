ff722f16bb5c07e65371a24013fb25bbe432dd50
ae-chutkul <alex.isara1925@gmail.com>
2025-09-24 09:56:53 +0700

    update 0956

--- convert-format-streamlit.py
+++ convert-format-streamlit.py
@@ -33,10 +33,10 @@ def login():
             st.error("❌ Invalid username or password")
     
     st.info(
-    "✨ **QuickWebP** supports instantly convert multiple images (JPG, JPEG, PNG) from several hundreds to thousand"
-    " into the modern **WebP format with single click**.\n\n"
-    "With WebP, you get **smaller file sizes** while keeping **high image quality** — "
-    "perfect for web use and faster sharing. 🚀"
+    "🚀 **QuickWebP** supports instantly convert multiple images (JPG, JPEG, PNG) from several hundreds to thousand"
+    " into the modern **WebP** format with single click.\n\n"
+    "In addition, with WebP, you get **smaller file sizes** while keeping **high image quality** — "
+    "perfect for using on web and faster sharing. ✨✨"
 )
 
 # Clear session state variables
