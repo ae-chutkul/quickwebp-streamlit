d781cd329df59a5c8b65d130fcb03179eb8e1f81
ae-chutkul <alex.isara1925@gmail.com>
2025-09-24 09:38:02 +0700

    update 0937

--- convert-format-streamlit.py
+++ convert-format-streamlit.py
@@ -217,7 +217,7 @@ def quickWebP():
     st.write("-----------------------------------------------------")
 
     # --- Logout ---
-    if st.button("🚪 Logout"):
+    if st.button(" Logout "):
         st.session_state.logged_in = False
         clear_all()
         st.rerun()
